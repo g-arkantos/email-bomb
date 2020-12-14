7f35de034b2144e1d3ff7e662aa57e09d0606a91
Paupar <64657169+g-arkantos@users.noreply.github.com>
2020-12-14 17:05:20 +0530

    Update emailbommer.py
    changes the counter variable from c to i.
    combined the subject and message using format string.
    Also, i thought using getpass library for taking password input would be nice!

--- emailbommer.py
+++ emailbommer.py
@@ -1,5 +1,6 @@
 import sys
 import smtplib
+import getpass
 print("+[+[+[+[+[[[[[let's Start.....]]]]+]+]+]+]")
 def lets():
 	try:
@@ -8,10 +9,11 @@ def lets():
 		amount=500
 		server='smtp.gmail.com'
 		port=587
-		fromadd=input("enter from address")
-		pwd=input("enter your password")
-		subject=input("enter subject")
-		message=input("enter message")
+		fromadd=input("enter from address: ")
+		pwd=getpass.getpass("Enter your password: ")
+		subject=input("enter subject: ")
+		msg=input("enter message: ")
+		message= f'Subject: {subject}\n\n\n{msg}'
 		s=smtplib.SMTP(server,port)
 		s.ehlo()
 		s.starttls()
@@ -19,7 +21,7 @@ def lets():
 		s.login(fromadd,pwd)
 		for i in range(amount+1):
 			s.sendmail(fromadd,target,message)
-			c+=1
+			i+=1
 			s.close()
 			sys.exit(0)
 	except Exception as e:
